e8c4bf7d9a1176a726d6a983afbed45bab66546d
Carol Dias <diascarolineg@gmail.com>
2016-06-27 04:35:55 +0000

    initial commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+	render() {
+		let txt = this.props.txt;
+		return <h1> {txt} </h1>
+	}
+}
+
+App.propTypes = {
+	txt: React.PropTypes.string,
+	cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+	txt: 'default prop for txt'
+}
+
+ReactDOM.render(
+	<App cat={5} txt="the props text value" />, 
+	document.getElementById('app')
+);
