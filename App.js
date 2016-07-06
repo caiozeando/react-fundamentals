0527542bc1736a2add4c67c10054d4e8c762f24f
Carol Dias <diascarolineg@gmail.com>
2016-07-06 15:00:21 +0000

    state components

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+	constructor() {
+		super();
+
+		this.state = { 
+			txt: 'txt state value',
+			cat: 123
+		}
+	}
+
+	update(e) {
+		this.setState({txt: e.target.value})
+	}
+
 	render() {
-		let txt = this.props.txt;
-		return <h1> {txt} </h1>
+		return (
+			<div>
+				<input type="text" onChange={this.update.bind(this)} />
+				<h1> { this.state.txt } </h1>
+			</div>
+		)
 	}
 }
 
-App.propTypes = {
-	txt: React.PropTypes.string,
-	cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-	txt: 'default prop for txt'
-}
-
-ReactDOM.render(
-	<App cat={5} txt="the props text value" />, 
-	document.getElementById('app')
-);
+export default App
