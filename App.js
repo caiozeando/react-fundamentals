75dd9a6444386f0ec88d81484eeac31078fffee6
Carol Dias <diascarolineg@gmail.com>
2016-07-06 21:07:55 +0000

    lifecycle - mounting basics

--- App.js
+++ App.js
@@ -1,17 +1,60 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+	constructor() {
+		super()
+		this.state = {val : 0}
+		this.update = this.update.bind(this)
+	}
+
+	update() {
+		this.setState({val: this.state.val + 1 })
+	}
+
+	componentWillMount() {
+		console.log('mounting...')
+	}
+
 	render() {
-		return <Button> We <Heart /> React </Button>
+		console.log('rendering!')
+		return <button className="btn btn-default" onClick={this.update}> {this.state.val} </button>
+	}
+
+	componentDidMount() {
+		console.log('mounted.')
+	}
+
+	componentWillUnmount() {
+		console.log('bye!')
 	}
 }
 
-class Button extends React.Component {
-	render() {
-		return <button className="btn btn-default"> {this.props.children} </button>
+
+class Wrapper extends React.Component {
+	constructor() {
+		super()
+	}
+
+	mount() {
+		ReactDOM.render(<App />, document.getElementById('btn-wrapper'))
+	}
+
+	unmount() {
+		ReactDOM.unmountComponentAtNode(document.getElementById('btn-wrapper'))
+	}
+
+	render(){
+		return (
+			<div>
+				<button className="btn btn-primary" onClick={this.mount.bind(this)}> Mount </button>
+				<button className="btn btn-danger" onClick={this.unmount.bind(this)}> Unmount </button>
+
+				<div id="btn-wrapper"></div>
+			</div>
+		)
 	}
 }
 
-const Heart = () => <span className="glyphicon glyphicon-heart"></span>
 
-export default App
+export default Wrapper
