769d7f4da0ef6bd190dc0549d60cbfe160e15d99
Carol Dias <diascarolineg@gmail.com>
2016-07-07 17:05:25 +0000

    higher order components

--- App.js
+++ App.js
@@ -1,40 +1,62 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-class App extends React.Component {
+let Mixin = InnerComponent => class extends React.Component {
 	constructor() {
 		super()
 
-		this.state = {increasing : false}
 		this.update = this.update.bind(this)
+		this.state = {val: 0}
 	}
 
 	update() {
-		ReactDOM.render(
-			<App val={this.props.val + 1} />,
-			document.getElementById('app')
-		);
+		this.setState({val: this.state.val + 1});
 	}
 
-	componentWillReceiveProps(nextProps) {
-		this.setState({increasing: nextProps.val > this.props.val})
-	}
-
-	shouldComponentUpdate(nextProps, nextState) {
-		return nextProps.val % 3 === 0
-	}
-
-	componentDidUpdate(prevProps, prevState) {
-		console.log('prevProps =>', prevProps)
-		console.log('prevState =>', prevState)
+	componentWillMount() {
+		console.log('will mount')
 	}
 
 	render() {
-		console.log(this.state.increasing)
-		return <button className="btn btn-default" onClick={this.update}>  {this.props.val} </button>
+		return (
+			<InnerComponent 
+				update={this.update}
+				{...this.state}
+				{...this.props}
+			/>
+		)
+	}
+
+	componentDidMount() {
+		console.log('mounted')
 	}
 }
 
-App.defaultProps = {val: 0}
+const Button = (props)  => 	<button 
+								className="btn btn-primary" 
+								onClick={props.update}> 
+									{props.txt}  - {props.val} 
+							</button>
+
+const Label = (props)  => 	<label 
+								className="label label-warning" 
+								onMouseMove={props.update}> 
+									{props.txt}  - {props.val} 
+							</label>
+
+
+let ButtonMixed = Mixin(Button)
+
+let LabelMixed = Mixin(Label)
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<ButtonMixed txt="Button"/>
+				<LabelMixed  txt="Label"/>
+			</div>
+		)
+	}
+}
 
 export default App
